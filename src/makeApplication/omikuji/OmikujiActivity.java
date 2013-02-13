55b49061ab8fa5cf7c18603c20ce07cd8ece0121
itacademy <lets@itacademy.jp>
2013-02-13 21:22:11 +0900

    アンドロイドおみくじ

---
+++ src/makeApplication/omikuji/OmikujiActivity.java
@@ -0,0 +1,114 @@
+package makeApplication.omikuji;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class OmikujiActivity extends Activity {
+
+	private OmikujiParts[] omikujiShelf = new OmikujiParts[20];
+
+	private OmikujiBox omikujibox = new OmikujiBox();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.omikuji);
+
+		this.omikujibox.setImageView((ImageView) findViewById(R.id.imageView1));
+		for (int i = 0; i < 20; i++) {
+			omikujiShelf[i] = new OmikujiParts(R.drawable.result2,
+					R.string.contents1);
+		}
+		omikujiShelf[0].drawID = R.drawable.result1;
+		omikujiShelf[0].fortuneID = R.string.contents2;
+
+		omikujiShelf[1].drawID = R.drawable.result3;
+		omikujiShelf[1].fortuneID = R.string.contents9;
+
+		omikujiShelf[2].fortuneID = R.string.contents3;
+		omikujiShelf[3].fortuneID = R.string.contents4;
+		omikujiShelf[4].fortuneID = R.string.contents5;
+		omikujiShelf[5].fortuneID = R.string.contents6;
+
+	}
+
+	public void onButtonClick(View v) {
+
+		this.omikujibox.shake();
+
+		/*
+		 * TranslateAnimation translate = new TranslateAnimation(0, 0, 0, -100);
+		 * translate.setRepeatMode(Animation.REVERSE);
+		 * translate.setRepeatCount(5); translate.setDuration(100);
+		 * 
+		 * ImageView imageView = (ImageView)findViewById(R.id.imageView1);
+		 * 
+		 * RotateAnimation rotate = new RotateAnimation(0, -36,
+		 * imageView.getWidth() / 2, imageView.getHeight() / 2);
+		 * rotate.setDuration(200);
+		 * 
+		 * AnimationSet set = new AnimationSet(true); set.addAnimation(rotate);
+		 * set.addAnimation(translate);
+		 * 
+		 * 
+		 * imageView.startAnimation(set);
+		 * 
+		 * /* ImageView image = new ImageView(this);
+		 * image.setImageResource(R.drawable.result1); setContentView(image);
+		 */
+
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			if (0 < this.omikujibox.getNumber()) {
+				this.drawResult();
+			}
+		}
+		return super.onTouchEvent(event);
+	}
+
+	public void drawResult() {
+
+		OmikujiParts op = omikujiShelf[omikujibox.getNumber()];
+
+		setContentView(R.layout.fortune);
+
+		ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+		imageView.setImageResource(op.drawID);
+
+		TextView textView = (TextView) findViewById(R.id.textView1);
+		textView.setTextColor(Color.BLACK);
+		textView.setText(op.fortuneID);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		
+		Toast toast = Toast.makeText(this, item.getTitle(), Toast.LENGTH_LONG);
+		toast.show();
+		
+		return super.onOptionsItemSelected(item);
+	}
+}
---
+++ src/makeApplication/omikuji/OmikujiBox.java
@@ -0,0 +1,76 @@
+package makeApplication.omikuji;
+
+import java.util.Random;
+
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+
+public class OmikujiBox implements AnimationListener {
+	private int number;// Ç≠Ç∂î‘çÜ
+	private ImageView imageView;
+
+	public void setImageView(ImageView imageView) {
+		this.imageView = imageView;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+	
+	public OmikujiBox() {
+		// TODO Auto-generated constructor stub
+		this. number = -1;
+	}
+	public void shake() {
+		TranslateAnimation translate = new TranslateAnimation(0, 0, 0, -100);
+    	translate.setRepeatMode(Animation.REVERSE);
+    	translate.setRepeatCount(5);
+    	translate.setDuration(100);
+    	
+    	
+    	RotateAnimation rotate = new RotateAnimation(0, -36,
+    			imageView.getWidth() / 2, imageView.getHeight() / 2);
+    	rotate.setDuration(200);
+    	
+    	AnimationSet set = new AnimationSet(true);
+    	set.addAnimation(rotate);
+    	set.addAnimation(translate);
+    	
+    	set.setAnimationListener(this);
+    	
+    	
+    	imageView.startAnimation(set);
+	}
+
+	@Override
+	public void onAnimationEnd(Animation animation) {
+		// TODO Auto-generated method stub
+		Random rud = new Random();
+		this.number = rud.nextInt(20);
+		Log.i("Omikuji", "number: " + this.number);
+		this.imageView.setImageResource(R.drawable.omikuji_result);
+		
+	}
+
+	@Override
+	public void onAnimationRepeat(Animation animation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onAnimationStart(Animation animation) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+
+	
+
+}
